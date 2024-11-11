2a26577f4ee8c93f96814589443d1c28183272e3
Eshow Lee <Eshow.Lee@advantech.com>
2024-11-11 08:52:54 +0800

    Update

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -167,14 +167,16 @@ namespace WPF
         // ---------------------------------------------------------------------------------------
         private void btn_test1_Click(object sender, RoutedEventArgs e)
         {
-            
+            // 
+
+
 
         }
 
         private void btn_test2_Click(object sender, RoutedEventArgs e)
         {
-            
-            
+
+
         }
     }
 }
